f0179e8e42f35b23bf6bef9e152ca2638b88e6c6
wxt9861 <wxt9861@icloud.com>
2021-04-07 16:11:21 -0400

    Update version #

--- custom_components/esxi_stats/const.py
+++ custom_components/esxi_stats/const.py
@@ -12,7 +12,7 @@ REQUIRED_FILES = [
     "services.yaml",
     "translations/en.json",
 ]
-VERSION = "0.5.5"
+VERSION = "0.6.0"
 ISSUE_URL = "https://github.com/wxt9861/esxi_stats/issues"
 
 STARTUP = """
